95d8136292dcd0c9e54dceff5904d0b30e289b1b
hryhorii_shkarta <grishkoshkarta1@gmail.com>
2025-12-27 13:02:30 +0200

    deleted log

--- src/components/Materials/Course/CoursePage/index.tsx
+++ src/components/Materials/Course/CoursePage/index.tsx
@@ -42,8 +42,6 @@ export default function CoursePageLayout({
     enabled: !!courseId,
   });
 
-  console.log('courseId', courseId);
-
   const deleteMutation = useMutation({
     mutationFn: () => deleteCourse(courseId),
     onSuccess: () => {
@@ -51,9 +49,6 @@ export default function CoursePageLayout({
       router.back();
     },
   });
-
-  console.log('isLoading', isLoading);
-  console.log('course', course);
 
   if (isLoading) return <Loader />;
 
